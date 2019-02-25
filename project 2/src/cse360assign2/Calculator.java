56ebb451ca92ead044e5d82365804644f2109f72
andre <andre@MSI>
2019-02-25 11:55:06 -0700

    Initial checkin of assign2
    Signed-off-by: andre <andre@MSI>

---
+++ project 2/src/cse360assign2/Calculator.java
@@ -0,0 +1,44 @@
+/**
+ * 	Name: Andrew T Nguyen
+ * 	Class: CSE 330 W-3:05pm
+ * 	Professor: Debra Calliss
+ *  Class ID: 505
+ *  Assignment 2
+ * 	Description: 
+ * 	
+ */
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
+
