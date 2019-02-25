acec34847217dfd2ddb3873cfc203819e57f9bb8
andre <andre@MSI>
2019-02-25 12:34:20 -0700

    First time change

--- project 2/src/cse360assign2/Calculator.java
+++ project 2/src/cse360assign2/Calculator.java
@@ -4,8 +4,8 @@
  * 	Professor: Debra Calliss
  *  Class ID: 505
  *  Assignment 2
- * 	Description: 
- * 	
+ * 	Description: This assignment is designed to practice with using a version control
+ * system with github. 
  */
 package cse360assign2;
 
@@ -13,30 +13,60 @@ public class Calculator {
 
 	private int total;
 	
+	/**
+	 * set total = 0 
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * 
+	 * @return total
+	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	/**
+	 * 
+	 * @param value
+	 */
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
+	/**
+	 * 
+	 * @param value
+	 */
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
+	/**
+	 * 
+	 * @param value
+	 */
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
+	/**
+	 * 
+	 * @param value
+	 */
 	public void divide (int value) {
-		
+		if (value == 0)
+			total = 0;
+		else
+			total = total / value;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getHistory () {
 		return "";
 	}
