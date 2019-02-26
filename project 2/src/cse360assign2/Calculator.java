29b421ac4ef727251be4da0e5645b9feb32118b1
andre <andre@MSI>
2019-02-25 21:34:52 -0700

    Third version, last change before submit

--- project 2/src/cse360assign2/Calculator.java
+++ project 2/src/cse360assign2/Calculator.java
@@ -1,5 +1,7 @@
+package cse360assign2;
+
 /**
- * 	Name: Andrew T Nguyen
+ * 	@author Andrew T Nguyen
  * 	Class: CSE 330 W-3:05pm
  * 	Professor: Debra Calliss
  *  Class ID: 505
@@ -7,21 +9,26 @@
  * 	Description: This assignment is designed to practice with using a version control
  * system with github. 
  */
-package cse360assign2;
 
 public class Calculator {
 
-	private int total;
+	private int total;					//declare the total variable
+	
+	private String theHistory;			//declare the string variable
 	
 	/**
-	 * set total = 0 
+	 * Calculator method, 
+	 * set total equal 0 
+	 * set history start from initial 0
 	 */
 	public Calculator () {
-		total = 0;  // not needed - included for clarity
+		total = 0;  		// not needed - included for clarity
+		
+		theHistory ="0"; 	// The history start from inital 0 value
 	}
 	
 	/**
-	 * 
+	 * getTotal method, return a total of all actions as a int value
 	 * @return total
 	 */
 	public int getTotal () {
@@ -29,31 +36,38 @@ public class Calculator {
 	}
 	
 	/**
-	 * 
+	 * Add method, adding value to the total
+	 * include that value to theHistory string
 	 * @param value
 	 */
 	public void add (int value) {
 		total = total + value;
+		theHistory = theHistory + "+" + value;
 	}
 	
 	/**
-	 * 
+	 * Subtract method, subtracting value to the total
+	 * include that value to theHistory string
 	 * @param value
 	 */
 	public void subtract (int value) {
 		total = total - value;
+		theHistory = theHistory + "-" + value;
 	}
 	
 	/**
-	 * 
+	 * Multiply method, multiplying value to the total
+	 * include that value to theHistory string
 	 * @param value
 	 */
 	public void multiply (int value) {
 		total = total * value;
+		theHistory = theHistory + "*" + value;
 	}
 	
 	/**
-	 * 
+	 * Divide method, dividing value to the total
+	 * include that value to theHistory string
 	 * @param value
 	 */
 	public void divide (int value) {
@@ -61,14 +75,16 @@ public class Calculator {
 			total = 0;
 		else
 			total = total / value;
+		
+		theHistory = theHistory + "/" + value;
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * getHistory method, return the history of all actions as a string
+	 * @return theHistory
 	 */
 	public String getHistory () {
-		return "";
+		return theHistory;
 	}
 }
 
